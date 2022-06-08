f2587d4850d7ad5d2a0a347bd60d04191a5e8dea
cverhaest <cverhaest@cverhaest-ltm5.internal.salesforce.com>
2022-06-08 10:35:19 +0200

    Workshop Automator sfdx-cli update crash commit

--- worker.js
+++ worker.js
@@ -12,6 +12,8 @@ const lineParse = require('./lib/lineParse');
 const setTimeoutPromise = util.promisify(setTimeout);
 const ex = 'deployMsg';
 
+// CVER - 20220607 - Changing debug level
+//logger.debug('I am a worker and I am up! yeah !');
 logger.info('I am a worker and I am up! yeah !');
 
 // load helpful plugins in cloud only
@@ -35,7 +37,9 @@ exec(cmd)
 })  // OK, we've got our environment prepared now.  Let's auth to our org and verify
 .then( (result) => {
 	if(process.env.DXLOGINURL){
-		logger.debug('destroy fs key file');
+		// CVER - 20220607 - Changing debug level
+		//logger.debug('destroy fs key file');
+		logger.info('destroy fs key file');
 		exec(`rm /app/tmp/devhub.key`);
 	}
 	logResult(result);
@@ -51,38 +55,54 @@ exec(cmd)
 				logger.error(err);
 			}
 			if (ok){
-				logger.debug(ok);
+				// CVER - 20220607 - Changing debug level
+				//logger.debug(ok);
+				logger.info(ok);
 			}
 		});
 		// ch.prefetch(1);
 
 		// this consumer eats deploys, creates local folders, and chops up the tasks into steps
 		ch.consume('deploys', (msg) => {
-			logger.debug('HEARD a message from the web');
+			// CVER - 20220607 - Changing debug level
+			//logger.debug('HEARD a message from the web');
+			logger.info('HEARD a message from the web');
 			const visitor = ua(process.env.UA_ID || 0);
 			// do a whole bunch of stuff here!
 	
-			logger.debug(msg);
+			// CVER - 20220607 - Changing debug level
+			//logger.debug(msg);
+			logger.info(msg);
 			const msgJSON = JSON.parse(msg.content.toString());
-			logger.debug(msgJSON);
-			logger.debug(msgJSON.deployId);
-			logger.debug(msgJSON.template);
+			// CVER - 20220607 - Changing debug level
+			//logger.debug(msgJSON);
+			//logger.debug(msgJSON.deployId);
+			//logger.debug(msgJSON.template);
+			logger.info(msgJSON);
+			logger.info(msgJSON.deployId);
+			logger.info(msgJSON.template);
 			visitor.event('Deploy Request', msgJSON.template).send();
 
 			// clone repo into local fs
 			// checkout only the specified branch, if specified
 			let gitCloneCmd;
 			if (msgJSON.branch){
-				logger.debug('It is a branch!');
+				// CVER - 20220607 - Changing debug level
+				//logger.debug('It is a branch!');
+				logger.info('It is a branch!');
 				gitCloneCmd = `cd tmp;git clone -b ${msgJSON.branch} --single-branch https://github.com/${msgJSON.username}/${msgJSON.repo}.git ${msgJSON.deployId}`;
-				logger.debug(gitCloneCmd);
+				// CVER - 20220607 - Changing debug level
+				//logger.debug(gitCloneCmd);
+				logger.info(gitCloneCmd);
 			} else {
 				gitCloneCmd = `cd tmp;git clone https://github.com/${msgJSON.username}/${msgJSON.repo}.git ${msgJSON.deployId}`;
 			}
 			exec(gitCloneCmd)
 			.then( (result) => {
 				// git outputs to stderr for unfathomable reasons
-				logger.debug(result.stderr);
+				// CVER - 20220607 - Changing debug level
+				//logger.debug(result.stderr);
+				logger.info(result.stderr);
 				ch.publish(ex, '', bufferKey(result.stderr, msgJSON.deployId));
 				return exec(`cd tmp;cd ${msgJSON.deployId};ls`);
 			})
@@ -91,31 +111,42 @@ exec(cmd)
 				// ch.publish(ex, '', bufferKey('Cloning the repository', msgJSON.deployId));
 				// ch.publish(ex, '', bufferKey(result.stdout, msgJSON.deployId));
 				// grab the deploy script from the repo
-				logger.debug(`going to look in the directory tmp/${msgJSON.deployId}/orgInit.sh`);
+				// CVER - 20220607 - Changing debug level
+				//logger.debug(`going to look in the directory tmp/${msgJSON.deployId}/orgInit.sh`);
+				logger.info(`going to look in the directory tmp/${msgJSON.deployId}/orgInit.sh`);
 
 				// use the default file if there's not one
 				if (!fs.existsSync(`tmp/${msgJSON.deployId}/orgInit.sh`)) {
 					const parsedLines = [];
-					logger.debug('no orgInit.sh.  Will use default');
+					// CVER - 20220607 - Changing debug level
+					//logger.debug('no orgInit.sh.  Will use default');
+					logger.info('no orgInit.sh.  Will use default');
 					parsedLines.push(`cd tmp;cd ${msgJSON.deployId};sfdx force:org:create -f config/project-scratch-def.json -s -d 1`);
 					parsedLines.push(`cd tmp;cd ${msgJSON.deployId};sfdx force:source:push`);
 					parsedLines.push(`cd tmp;cd ${msgJSON.deployId};sfdx force:org:open`);
 					return parsedLines;
 				} else { // read the lines
-					logger.debug('found a orgInit.sh');
+					// CVER - 20220607 - Changing debug level
+					//logger.debug('found a orgInit.sh');
+					logger.info('found a orgInit.sh');
 					return lineParse(msgJSON, ch, visitor);
 				}
 			})
 			.then( (parsedLines) => {
-				logger.debug('these are the parsed lines:');
-				logger.debug(parsedLines);
+				// CVER - 20220607 - Changing debug level
+				//logger.debug('these are the parsed lines:');
+				//logger.debug(parsedLines);
+				logger.info('these are the parsed lines:');
+				logger.info(parsedLines);
 				// some kind of error occurred, already handled
 				if (!parsedLines) {
 					logger.error('line parsing failed');
 					ch.ack(msg);
 					return;
 				} else {
-					logger.debug('got back parsed lines');
+					// CVER - 20220607 - Changing debug level
+					//logger.debug('got back parsed lines');
+					logger.info('got back parsed lines');
 					let localLineRunner = new lineRunner(msgJSON, parsedLines, ch, visitor);
 					return localLineRunner.runLines();	
 				}
@@ -130,6 +161,7 @@ exec(cmd)
 				return setTimeoutPromise(1000 * 60, 'foobar');
 			})
 			.then(() => {
+				logger.info('execute rm -rf');
 				exec(`cd tmp;rm -rf ${msgJSON.deployId}`);
 			})
 			.then((cleanResult) => {
